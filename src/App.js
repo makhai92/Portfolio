20d4c663ef98f93265175453bb8f297b8984ce56
makhai92 <makkai9210@gmail.com>
2025-02-12 12:05:07 +0900

    스크롤투탑 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/main/Main";
 import Menu from "./components/menu/Menu";
-import ScrollToTop from "./components/utils/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
-      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/menu" element={<Menu />} />
